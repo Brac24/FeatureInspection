be31a4c354b3fcb544c348d2fe4d035bd85401f5
Ian Pommer <pommpan@gmail.com>
2017-08-16 11:48:50 -0700

    Trying to get back

--- FeatureCreationTableMock.Designer.cs
+++ FeatureCreationTableMock.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 462);
+            this.ClientSize = new System.Drawing.Size(700, 412);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FeatureCreationTableMock";
             this.Text = "FeatureCreationTableMock";
