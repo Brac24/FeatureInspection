769e3bdca4caa47c15ef95f174c49c4d01452be4
Christian Bracamontes <brac24@live.com>
2017-08-30 15:49:30 -0700

    merged with master added method from old branch

--- FeatureCreationModelMock.cs
+++ FeatureCreationModelMock.cs
@@ -115,7 +115,30 @@ namespace Feature_Inspection
             return t;
 
         }
-        
+
+        public DataTable GetFeaturesOnOpKey(int opKey)
+        {
+            DataTable t;
+            OdbcDataAdapter dataAdapter;
+            using (OdbcConnection conn = new OdbcConnection(connection_string))
+            using (OdbcCommand com = conn.CreateCommand())
+            using (OdbcDataAdapter adapter = new OdbcDataAdapter(com))
+            {
+                string query = "SELECT * FROM ATI_FeatureInspection.dbo.Features WHERE Part_Number_FK = (SELECT Part_Number FROM ATI_FeatureInspection.dbo.Operation WHERE Op_Key =  " + opKey + ") AND Operation_Number_FK = (SELECT Operation_Number FROM ATI_FeatureInspection.dbo.Operation WHERE Op_Key = " + opKey + ");";
+
+                dataAdapter = adapter;
+
+
+                com.CommandText = query;
+                t = new DataTable();
+                dataAdapter.Fill(t);
+
+            }
+
+            return t;
+
+        }
+
 
         // INSPECTION MODEL
 
